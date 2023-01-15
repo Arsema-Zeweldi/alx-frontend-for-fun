e1535e364fff1af5a482edc9cb107b5d633c5ed3
Arsema-Zeweldi <arsugeb@gmail.com>
2023-01-15 12:39:47 +0300

    fix 5

--- markdown2html.py
+++ markdown2html.py
@@ -7,9 +7,8 @@ import os
 
 if __name__ == "__main__":
     def errprint(*args):
-    """print errors"""
+        """print errors"""
         print(*args, file=sys.stderr)
-
 
     if len(sys.argv) < 3:
         errprint("Usage: ./markdown2html.py README.md README.html")
@@ -18,4 +17,3 @@ if __name__ == "__main__":
     if not os.path.exists(sys.argv[1]):
         errprint("Missing {}".format(sys.argv[1]))
         exit(1)
-	exit(0)
