20a1fe83ce054a3cf98911c0dee2056ce73e3048
Arsema-Zeweldi <arsugeb@gmail.com>
2023-01-15 12:13:01 +0300

    fix

--- markdown2html.py
+++ markdown2html.py
@@ -8,18 +8,21 @@ import markdown
 from mdutils.mdutils import MdUtils
 from mdutils import Html
 
+def createHtml():
+    def errprint(*args):
+        print(*args, file=sys.stderr)
 
-def errprint(*args):
-    print(*args, file=sys.stderr)
 
+    if len(sys.argv) < 3:
+        errprint("Usage: ./markdown2html.py README.md README.html")
+        exit(1)
 
-if len(sys.argv) < 3:
-    errprint("Usage: ./markdown2html.py README.md README.html")
-    raise SystemExit(1)
-else:
-    path = "./"
     if os.path.exists(sys.argv[1]):
         markdown.markdownFromFile(input=sys.argv[1], output=sys.argv[2])
     else:
         errprint("Missing ", sys.argv[1])
-        raise SystemExit(1)
+        exit(1)
+
+
+if __name__ == "__main__":
+    createHtml()
