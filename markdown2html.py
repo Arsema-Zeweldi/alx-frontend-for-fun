ba860e786b107311123778a8b2de500fdba42885
Arsema-Zeweldi <arsugeb@gmail.com>
2023-01-15 12:59:36 +0300

    Task 1

--- markdown2html.py
+++ markdown2html.py
@@ -4,6 +4,7 @@
 
 import sys
 import os
+import markdown
 
 if __name__ == "__main__":
     def errprint(*args):
@@ -17,3 +18,13 @@ if __name__ == "__main__":
     if not os.path.exists(sys.argv[1]):
         errprint("Missing {}".format(sys.argv[1]))
         exit(1)
+
+    readme = open(sys.argv[1], 'r')
+    for line in readme:
+        lenOfLine = len(line)
+        heading = line.lstrip("#")
+        numOfHeading = lenOfLine - len(heading)
+
+        if 1 <= numOfHeading <= 6:
+            line = "<h{}>".format(numOfHeading) + heading.strip() +
+            "</h{}>\n".format(numOfHeading)
